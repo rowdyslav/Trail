ce05777a0350aa0e86a07e89e3880a87e98902fa
rowdyslav <rowdyslav@gmail.com>
2026-03-28 11:31:14 +0300

    Add route_type filter to GET /routes

--- backend/routers/routes.py
+++ backend/routers/routes.py
@@ -1,16 +1,22 @@
+from typing import Annotated
+
 from beanie import PydanticObjectId
-from fastapi import APIRouter
+from fastapi import APIRouter, Query
 
 from core.api.errors import ber, route_not_found_error
 from core.api.schemas import RouteRead
+from core.domain.rewards import RouteType
 from core.models import Route
 
 router = APIRouter(prefix="/routes", tags=["Routes"])
 
 
 @router.get("")
-async def read_routes() -> list[RouteRead]:
-    routes = await Route.find(fetch_links=True).to_list()
+async def read_routes(
+    route_type: Annotated[RouteType | None, Query()] = None,
+) -> list[RouteRead]:
+    filters = {} if route_type is None else {"route_type": route_type}
+    routes = await Route.find(filters, fetch_links=True).to_list()
     return [route.to_read() for route in routes]
 
 
